654d57d7f88d6dead9f5d69e6a3341de16d5e281
Yuanjun Zhong <zhuimengzyj@gmail.com>
2021-08-14 19:27:46 +0800

    complete ch02

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,32 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    let secret_number = rand::thread_rng().gen_range(1..101);
+    println!("Guess the number!");
+    loop {
+        println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            // readline 是append 用户输入到 guess
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please input a number.");
+                continue;
+            }
+        };
+        println!("You guessed: {}", guess);
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too large!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+}
