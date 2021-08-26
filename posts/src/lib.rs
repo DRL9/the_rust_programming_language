8c99097c5f587e1dd6a9e3f872bbc4d6137af43c
Yuanjun Zhong <zhuimengzyj@gmail.com>
2021-08-26 21:35:51 +0800

    complete ch18

---
+++ posts/src/lib.rs
@@ -0,0 +1,78 @@
+trait State {
+    fn request_review(self: Box<Self>) -> Box<dyn State>;
+    fn approve(self: Box<Self>) -> Box<dyn State>;
+    fn content<'a>(&self, _: &'a Post) -> &'a str {
+        ""
+    }
+}
+
+struct Draft {}
+impl State for Draft {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        Box::new(PendingReview {})
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+}
+
+struct PendingReview {}
+
+impl State for PendingReview {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        Box::new(Published {})
+    }
+}
+
+struct Published {}
+
+impl State for Published {
+    fn request_review(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn approve(self: Box<Self>) -> Box<dyn State> {
+        self
+    }
+
+    fn content<'a>(&self, post: &'a Post) -> &'a str {
+        post.content.as_str()
+    }
+}
+pub struct Post {
+    content: String,
+    state: Option<Box<dyn State>>,
+}
+
+impl Post {
+    pub fn new() -> Self {
+        Post {
+            content: String::from(""),
+            state: Some(Box::new(Draft {})),
+        }
+    }
+    pub fn content(&mut self) -> &str {
+        self.state.as_ref().unwrap().content(self)
+    }
+
+    pub fn add_text(&mut self, s: &str) {
+        self.content.push_str(s);
+        self.state = Some(Box::new(Draft {}));
+    }
+    pub fn request_review(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.request_review());
+        }
+    }
+
+    pub fn approve(&mut self) {
+        if let Some(s) = self.state.take() {
+            self.state = Some(s.approve());
+        }
+    }
+}
